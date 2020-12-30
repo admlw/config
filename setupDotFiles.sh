c1b96ce7a34e056a2a488e1104249ec8ef27fb0e
Adam Lister <adamlister@Adams-MacBook-Pro.local>
2020-12-29 23:08:20 -0600

    updates for working with zsh, remove some vim plugins

--- setupDotFiles.sh
+++ setupDotFiles.sh
@@ -1,4 +1,13 @@
-cp .bashrc ~/
-cp .bash_profile ~/
+# check shell
+if [[ ${SHELL} == "/bin/zsh" ]]; then
+  echo "setting up for zsh shell"
+  cp .rc ~/.zshrc
+  cp .profile ~/.profile
+else
+  echo "setting up for bash shell"
+  cp .rc ~/.bashrc
+  cp .profile ~/
+fi
+
 cp .tmux.conf ~/
 cp .vimrc ~/  
